6d5371a35708327925b8348f114b1b827833e05d
helia1385mir <88113615+helia1385mir@users.noreply.github.com>
2021-08-10 12:34:35 +0430

    Add files via upload

---
+++ kmm.py
@@ -0,0 +1,13 @@
+a=int(input()) 
+b=int(input())
+h=[]
+for i in range(1,a) :
+    if a %i==0 and b%i==0 :   
+        h=[i]
+        
+bmm=max(h)
+if a>b :
+    int(input(print('dobare add bezarid add aval bayad add kochak bashad')))
+
+kmm=(a*b)/bmm
+print(kmm)
